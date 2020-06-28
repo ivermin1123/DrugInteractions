9c66b0994b29ce4ef61abde98ea367e2214c4fe0
Hoang Le <mrkjin99@gmail.com>
2020-06-28 19:44:17 +0700

    fix error wrong path folder UI

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -24,7 +24,7 @@ namespace WebAPI
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/dist";
+                configuration.RootPath = "UI/dist";
             });
             services.AddSingleton<IConfiguration>(Configuration);
         }
@@ -64,7 +64,7 @@ namespace WebAPI
                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
-                spa.Options.SourcePath = "ClientApp";
+                spa.Options.SourcePath = "UI";
 
                 if (env.IsDevelopment())
                 {
